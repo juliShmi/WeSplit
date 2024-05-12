c41420a70c89e0ed781331a89478aac24941b062
iuliia.shmitko <iuliia.shmitko@telekom.com>
2024-05-12 15:36:49 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Iuliia Shmitko on 12/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
